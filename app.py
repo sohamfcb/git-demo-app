81ef362dc8d293bbaefc0a232a263b4ce3b8a02f
sohamfcb <soumyapu31@gmail.com>
2023-11-14 18:20:13 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Messi')
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('messi.jpg')
+
+with col2:
+    st.write('''He was beautiful. He was the point of difference, he has always been the point of difference. Unparalleled. And maybe today there will of course always be those who argue, always be those who debate. And the debate can rage on if you like, but as he falls in love with the object in the world that his heart most desired, it’s hard to escape the supposition that he has rendered himself today, the 𝗚𝗥𝗘𝗔𝗧𝗘𝗦𝗧 𝗢𝗙 𝗔𝗟𝗟 𝗧𝗜𝗠𝗘.''')
